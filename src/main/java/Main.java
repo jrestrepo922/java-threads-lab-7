8117fcc262352746da2fe98f9117fa134f4ff6f7
Juan Martin Restrepo <tinto922@gmail.com>
2022-07-27 11:26:08 -0400

    complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,19 @@ import java.util.concurrent.Callable;
 public class Main {
     public static Callable<Integer> getCallableInteger() {
         // your code here
+        try(Scanner scanner = new Scanner(System.in)){
+            Integer num = scanner.nextInt();
+
+            Callable<Integer> numCallable = () -> {
+                return num;
+            };
+            return numCallable;
+        }
+
     }
+
+    public static void main(String[] args) {
+        System.out.println(Main.getCallableInteger());
+    }
+
 }
